d68e5890483cfe6630d6e495a7476b47f111c2a7
JonBeardDev <jon.beard@gmail.com>
2023-03-13 11:42:34 -0500

    Fixed Add cards link on Not Enough page

--- src/Decks/NotEnough.js
+++ src/Decks/NotEnough.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 import { PlusCircle } from "react-bootstrap-icons";
 
 // Function displays a message when the user tries to study from a deck with <3 cards
 function NotEnough({ cards }) {
+  // Pull deck id from parameters
+  const { deckId } = useParams();
+
   // Set message (ensures correct grammar is used for 0, 1, and 2 cards in deck)
   let howManyCards = "";
   if (cards === 1) howManyCards = "There is 1 card";
@@ -19,9 +23,11 @@ function NotEnough({ cards }) {
       </div>
       <div>
         {/* Add Cards button sends user to AddCard page for this deck */}
-        <button className="btn btn-lg btn-primary">
-          <PlusCircle style={{ verticalAlign: -3 }} /> Add Cards
-        </button>
+        <Link to={`/decks/${deckId}/cards/new`}>
+          <button className="btn btn-lg btn-primary">
+            <PlusCircle style={{ verticalAlign: -3 }} /> Add Cards
+          </button>
+        </Link>
       </div>
     </div>
   );
